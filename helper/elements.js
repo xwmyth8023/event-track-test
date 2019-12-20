50a93d62c4f2b5bee1824008a6fc5c44e317da09
Sandy <sandy@sandy.intranet.theknot.com>
2019-12-20 18:38:47 +0800

    add element data

---
+++ helper/elements.js
@@ -0,0 +1,290 @@
+'use strict';
+
+module.exports = {
+  bumpLayoutHeader: {
+    bumpLogo: '#main-container > div:nth-child(1) > div > div > a',
+    searchTheSite: '#main-container > div:nth-child(1) > div > div > div.Header-module__desktopHeaderSearchContainer___1I6wl > input',
+    facebook: '#main-container > div:nth-child(1) > div > div > div.Header-module__desktopHeaderRightColumn___1QXMb > a:nth-child(1)',
+    instagram: '#main-container > div:nth-child(1) > div > div > div.Header-module__desktopHeaderRightColumn___1QXMb > a:nth-child(2)',
+    pinterest: '#main-container > div:nth-child(1) > div > div > div.Header-module__desktopHeaderRightColumn___1QXMb > a:nth-child(3)',
+    twitter:'#main-container > div:nth-child(1) > div > div > div.Header-module__desktopHeaderRightColumn___1QXMb > a:nth-child(4)',
+    login: '#main-container > div:nth-child(1) > div > div > div.Header-module__desktopHeaderRightColumn___1QXMb > a.Header-module__headerLogin___3gDNu.Header-module__logout___HFWq1'
+  },
+  bumpLayoutNav: {
+    babyRegistry: '#desktop-left-nav > nav > div > div:nth-child(1) > div > a',
+    babyRegistryFinder: '#desktop-left-nav > nav > div > div:nth-child(1) > a',
+    realAnswers: '#desktop-left-nav > nav > div > div:nth-child(2) > div > a',
+    gettingPregnant: '#desktop-left-nav > nav > div > div:nth-child(3) > div > a',
+    gettingPregnantDropdown: '#desktop-left-nav > nav > div > div:nth-child(3) > div > span',
+    signsOfOvulation: '#desktop-left-nav > nav > div > div:nth-child(3) > a:nth-child(2)',
+    ovulationTracker: '#desktop-left-nav > nav > div > div:nth-child(3) > a:nth-child(3)',
+    pregnancy: '#desktop-left-nav > nav > div > div:nth-child(4) > div > a',
+    firstTrimester: '#desktop-left-nav > nav > div > div:nth-child(4) > a:nth-child(2)',
+    secondTrimester: '#desktop-left-nav > nav > div > div:nth-child(4) > a:nth-child(3)',
+    thirdTrimester: '#desktop-left-nav > nav > div > div:nth-child(4) > a:nth-child(4)',
+    pregnancyWeekByWeek: '#desktop-left-nav > nav > div > div:nth-child(4) > a:nth-child(5)',
+    pregnancyShowers: '#desktop-left-nav > nav > div > div:nth-child(4) > a:nth-child(6)',
+    bestOfBabyList: '#desktop-left-nav > nav > div > div:nth-child(4) > a:nth-child(7)',
+    baby: '#desktop-left-nav > nav > div > div:nth-child(5) > div > a',
+    babyDropdown: '#desktop-left-nav > nav > div > div:nth-child(5) > div > span',
+    breastfeeding: '#desktop-left-nav > nav > div > div:nth-child(5) > a:nth-child(2)',
+    babyMonthByMonth: '#desktop-left-nav > nav > div > div:nth-child(5) > a:nth-child(3)',
+    toddler: '#desktop-left-nav > nav > div > div:nth-child(6) > div > a',
+    toddlerMonthByMonth: '#desktop-left-nav > nav > div > div:nth-child(6) > a',
+    babyNames: '#desktop-left-nav > nav > div > div:nth-child(7) > div > a',
+    babyNamesDropdown: '#desktop-left-nav > nav > div > div:nth-child(7) > div > span',
+    babyNamesHome: '#desktop-left-nav > nav > div > div:nth-child(7) > a:nth-child(2)',
+    babyGirlNames: '#desktop-left-nav > nav > div > div:nth-child(7) > a:nth-child(3)',
+    babyBoyNames: '#desktop-left-nav > nav > div > div:nth-child(7) > a:nth-child(4)',
+    babyNameLists: '#desktop-left-nav > nav > div > div:nth-child(7) > a:nth-child(5)',
+    babyNamesByOrigin: '#desktop-left-nav > nav > div > div:nth-child(7) > a:nth-child(6)',
+    viewAllTopics: '#desktop-left-nav > nav > div > div:nth-child(8) > div > a',
+    tools: '#desktop-left-nav > nav > div > div:nth-child(9) > div > a',
+    toolsDropdown: '#desktop-left-nav > nav > div > div:nth-child(9) > div > span',
+    chineseGenderChart: '#desktop-left-nav > nav > div > div:nth-child(9) > a:nth-child(2)',
+    viewAllTools: '#desktop-left-nav > nav > div > div:nth-child(9) > a:nth-child(3)',
+    news: '#desktop-left-nav > nav > div > div:nth-child(10) > div > a',
+    community: '#desktop-left-nav > nav > div > div:nth-child(11) > div > a',
+    videos: '#desktop-left-nav > nav > div > div:nth-child(12) > div > a',
+    accountMore: '#desktop-left-nav > nav > div > div:nth-child(13) > div.Nav-module__headerContainer___2w2xT > a',
+    accountDropdown: '#desktop-left-nav > nav > div > div:nth-child(13) > div.Nav-module__headerContainer___2w2xT > span',
+    login: '#desktop-left-nav > nav > div > div:nth-child(13) > div:nth-child(2) > a:nth-child(1)',
+    createAnAccount: '#desktop-left-nav > nav > div > div:nth-child(13) > div:nth-child(2) > a:nth-child(2)'
+  },
+  bumpLayoutFooter: {
+    abouteUsAndContact: '#responsive-footer > div > footer > div > div:nth-child(1) > div.Footer-module__flexInitial___1Y80D > div > div.Footer-module__flexGroup___20gCt.Footer-module__marginBottom140px___1c5sP > a:nth-child(1)',
+    advertiseWithUs: '#responsive-footer > div > footer > div > div:nth-child(1) > div.Footer-module__flexInitial___1Y80D > div > div.Footer-module__flexGroup___20gCt.Footer-module__marginBottom140px___1c5sP > a:nth-child(2)',
+    jobs: '#responsive-footer > div > footer > div > div:nth-child(1) > div.Footer-module__flexInitial___1Y80D > div > div.Footer-module__flexGroup___20gCt.Footer-module__marginBottom140px___1c5sP > a:nth-child(3)',
+    privacyPolicy: '#responsive-footer > div > footer > div > div:nth-child(1) > div.Footer-module__flexInitial___1Y80D > div > div.Footer-module__flexGroup___20gCt.Footer-module__marginBottom140px___1c5sP > a:nth-child(4)',
+    termsOfUse: '#responsive-footer > div > footer > div > div:nth-child(1) > div.Footer-module__flexInitial___1Y80D > div > div.Footer-module__flexGroup___20gCt.Footer-module__marginBottom140px___1c5sP > a:nth-child(5)',
+    thebumpIos: '#responsive-footer > div > footer > div > div:nth-child(2) > div:nth-child(1) > div.Footer-module__flexGroup___20gCt.Footer-module__linkGroup___2-m3F > a:nth-child(2)',
+    thebumpAndroid: '#responsive-footer > div > footer > div > div:nth-child(2) > div:nth-child(1) > div.Footer-module__flexGroup___20gCt.Footer-module__linkGroup___2-m3F > a:nth-child(3)',
+    followFacebook: '#responsive-footer > div > footer > div > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > a:nth-child(2)',
+    followInstagram: '#responsive-footer > div > footer > div > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > a:nth-child(3)',
+    followPinterest: '#responsive-footer > div > footer > div > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > a:nth-child(4)',
+    followTwitter: '#responsive-footer > div > footer > div > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > a:nth-child(5)',
+    theKnotLogo: '#responsive-footer > div > footer > div > div:nth-child(2) > div:nth-child(1) > div:nth-child(3) > a:nth-child(2) > svg',
+    theNestLogo: '#responsive-footer > div > footer > div > div:nth-child(2) > div:nth-child(1) > div:nth-child(3) > a:nth-child(3) > svg',
+    theBashLogo: '#responsive-footer > div > footer > div > div:nth-child(2) > div:nth-child(1) > div:nth-child(3) > a:nth-child(4) > svg',
+    topArticles:{
+      babyShowerGames: { 
+        locator:'#responsive-footer > div > footer > div > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > a:nth-child(2)',
+        selection: 'baby shower games'
+      },
+      pregnancyFears: { 
+        locator:'#responsive-footer > div > footer > div > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > a:nth-child(3)',
+        selection: 'pregnancy fears'
+      },
+      bestPregnancyTests: {
+        locator:'#responsive-footer > div > footer > div > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > a:nth-child(4)',
+        selection:'best pregnancy tests'
+      },
+      earlySignsOfPregnancy: {
+        locator:'#responsive-footer > div > footer > div > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > a:nth-child(5)',
+        selection:'early signs of pregnancy'
+      },
+      nurseryIdeas: {
+        locator:'#responsive-footer > div > footer > div > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > a:nth-child(6)',
+        selection:'nursery ideas'
+      },
+      genderRevealIdeas: {
+        locator:'#responsive-footer > div > footer > div > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > a:nth-child(7)',
+        selection:'gender reveal ideas'
+      }
+    },
+    registry:{
+      babyRegistryOnFooter: {
+        locator:'#responsive-footer > div > footer > div > div:nth-child(2) > div:nth-child(2) > div:nth-child(2) > a:nth-child(2)',
+        selection:'baby registry'
+      },
+      babyRegistryChecklist: {
+        locator:'#responsive-footer > div > footer > div > div:nth-child(2) > div:nth-child(2) > div:nth-child(2) > a:nth-child(3)',
+        selection:'baby registry checklist'
+      },
+      giftForNewMoms: {
+        locator:'#responsive-footer > div > footer > div > div:nth-child(2) > div:nth-child(2) > div:nth-child(2) > a:nth-child(4)',
+        selection:'gifts for new moms'
+      },
+      bestStrollers: {
+        locator:'#responsive-footer > div > footer > div > div:nth-child(2) > div:nth-child(2) > div:nth-child(2) > a:nth-child(5)',
+        selection:'best strollers'
+      },
+      bestDoubleStrollers: {
+        locator:'#responsive-footer > div > footer > div > div:nth-child(2) > div:nth-child(2) > div:nth-child(2) > a:nth-child(6)',
+        selection:'best double strollers'
+      },
+      bestCribs: {
+        locator:'#responsive-footer > div > footer > div > div:nth-child(2) > div:nth-child(2) > div:nth-child(2) > a:nth-child(7)',
+        selection:'best cribs'
+      },
+      BestCarSeats: {
+        locator:'#responsive-footer > div > footer > div > div:nth-child(2) > div:nth-child(2) > div:nth-child(2) > a:nth-child(8)',
+        selection:'best car seats'
+      },
+    },
+    toolsAndResources:{
+      pregnancyWeekByWeekOnFooter: {
+        locator:'#responsive-footer > div > footer > div > div:nth-child(2) > div:nth-child(3) > div > a:nth-child(2)',
+        selection:'pregnancy week by week'
+      },
+      howBigIsBaby: {
+        locator:'#responsive-footer > div > footer > div > div:nth-child(2) > div:nth-child(3) > div > a:nth-child(3)',
+        selection:'how big is baby'
+      },
+      hospitalBagChecklist: {
+        locator:'#responsive-footer > div > footer > div > div:nth-child(2) > div:nth-child(3) > div > a:nth-child(4)',
+        selection:'hospital bag checklist'
+      },
+      babyNamesOnFooter: {
+        locator:'#responsive-footer > div > footer > div > div:nth-child(2) > div:nth-child(3) > div > a:nth-child(5)',
+        selection:'baby names'
+      },
+      babyBoyNamesOnFooter: {
+        locator:'#responsive-footer > div > footer > div > div:nth-child(2) > div:nth-child(3) > div > a:nth-child(6)',
+        selection:'baby boy names'
+      },
+      babyGirlNamesOnFooter: {
+        locator:'#responsive-footer > div > footer > div > div:nth-child(2) > div:nth-child(3) > div > a:nth-child(7)',
+        selection:'baby girl names'
+      },
+      uniqueBabyNamesOnFooter: {
+        locator:'#responsive-footer > div > footer > div > div:nth-child(2) > div:nth-child(3) > div > a:nth-child(8)',
+        selection:'unique baby names'
+      },
+      chineseGenderChartOnFooter: {
+        locator:'#responsive-footer > div > footer > div > div:nth-child(2) > div:nth-child(3) > div > a:nth-child(9)',
+        selection:'chinese gender chart'
+      },
+      amIPregnantQuiz: {
+        locator:'#responsive-footer > div > footer > div > div:nth-child(2) > div:nth-child(3) > div > a:nth-child(10)',
+        selection:'am i pregnant quiz'
+      },
+      dueDateCalculator: {
+        locator:'#responsive-footer > div > footer > div > div:nth-child(2) > div:nth-child(3) > div > a:nth-child(11)',
+        selection:'due date calculator'
+      },
+      ovulationCalendar: {
+        locator:'#responsive-footer > div > footer > div > div:nth-child(2) > div:nth-child(3) > div > a:nth-child(12)',
+        selection:'ovulation calendar'
+      },
+      contractionTimer: {
+        locator:'#responsive-footer > div > footer > div > div:nth-child(2) > div:nth-child(3) > div > a:nth-child(13)',
+        selection:'contraction timer'
+      },
+      breastfeedingonFooter: {
+        locator:'#responsive-footer > div > footer > div > div:nth-child(2) > div:nth-child(3) > div > a:nth-child(14)',
+        selection:'breastfeeding'
+      },
+      communityGlossary: {
+        locator:'#responsive-footer > div > footer > div > div:nth-child(2) > div:nth-child(3) > div > a:nth-child(15)',
+        selection:'community glossary'
+      },
+      living: {
+        locator:'#responsive-footer > div > footer > div > div:nth-child(2) > div:nth-child(3) > div > a:nth-child(16)',
+        selection:'living'
+      }
+    }
+  },
+  detailArticle:{
+    shareToPinterest:{
+      locator: '#content-container > div.largeImageHeader_a1d3f.desktop_a1d3f > div.imageContainer_a1d3f > a',
+      service: 'pinterest'
+    },
+    shareToFacebook:{
+      locator: '#content-container > div.largeImageHeader_a1d3f.desktop_a1d3f > div.sandBackground_b0bc8.desktop_b0bc8 > div.article_b0bc8 > div.shareContainer_b0bc8 > a:nth-child(1)',
+      service: 'facebook'
+    },
+    shareToEmail:{
+      locator: '#content-container > div.largeImageHeader_a1d3f.desktop_a1d3f > div.sandBackground_b0bc8.desktop_b0bc8 > div.article_b0bc8 > div.shareContainer_b0bc8 > a.shareBtnEmail_b0bc8.shareIcon_b0bc8',
+      service: 'email'
+    },
+    shareFacebookBtn: {
+      locator: '#content-container > div.contentContainer_fcba6 > div.articlesContentContainer_f683b.desktop_f683b > div:nth-child(1) > div.share_44bb5.desktop_44bb5 > div > div > a.shareIcon_44bb5.shareBtnFacebook_44bb5',
+      service: 'facebook'
+    },
+    shareTwitterBtn: {
+      locator: '#content-container > div.contentContainer_fcba6 > div.articlesContentContainer_f683b.desktop_f683b > div:nth-child(1) > div.share_44bb5.desktop_44bb5 > div > div > a.shareIcon_44bb5.shareBtnTwitter_44bb5',
+      service: 'twitter'
+    },
+    copyLinkBtn: {
+      locator: '#content-container > div.contentContainer_fcba6 > div.articlesContentContainer_f683b.desktop_f683b > div:nth-child(1) > div.share_44bb5.desktop_44bb5 > div > div > a.shareIcon_44bb5.copyLink_44bb5.shareDesktop_44bb5',
+      service: 'link'
+    },
+    shareEmailBtn: {
+      locator: '#content-container > div.contentContainer_fcba6 > div.articlesContentContainer_f683b.desktop_f683b > div:nth-child(1) > div.share_44bb5.desktop_44bb5 > div > div > a.shareIcon_44bb5.email_44bb5',
+      service: 'email'
+    }
+  },
+  registryRetailer:{
+    startAmazon:{
+      locator: '#content-container > div > div.retailers-container > div > div:nth-child(1) > a',
+      retailerName: "Amazon",
+      source: "registry hub"
+    },
+    startPotteryBarnBaby:{
+      locator: '#content-container > div > div.retailers-container > div > div:nth-child(2) > a',
+      retailerName: 'POTTERY BARN BABY',
+      source: "registry hub"
+    },
+    startBuyBuyBaby:{
+      locator: '#content-container > div > div.retailers-container > div > div:nth-child(3) > a',
+      retailerName: 'buybuy BABY',
+      source: "registry hub"
+    },
+    startCrateAndKids:{
+      locator: '#content-container > div > div.retailers-container > div > div:nth-child(4) > a',
+      retailerName: 'Crate & Kids',
+      source: "registry hub"
+    },
+    startWalmart:{
+      locator: '#content-container > div > div.retailers-container > div > div:nth-child(5) > a',
+      retailerName: 'Walmart',
+      source: "registry hub"
+    },
+    startTarget: {
+      locator: '#content-container > div > div.retailers-container > div > div:nth-child(6) > a',
+      retailerName: 'Target',
+      source: "registry hub"
+    },
+    amazonRetailer:{
+      locator: '#all-retailers > div > div:nth-child(1) > a',
+      retailerName: 'Amazon',
+      source: 'registry grid'
+    },
+    potterBarnBabyRetailer:{
+      locator: '#all-retailers > div > div:nth-child(2) > a',
+      retailerName: 'POTTERY BARN BABY',
+      source: 'registry grid'
+    },
+    buyBuyBabyRetailer:{
+      locator: '#all-retailers > div > div:nth-child(3) > a',
+      retailerName: 'buybuy BABY',
+      source: 'registry grid'
+    },
+    crateAndKidsRetailer:{
+      locator: '#all-retailers > div > div:nth-child(4) > a',
+      retailerName: 'Crate & Kids',
+      source: 'registry grid'
+    },
+    walmartRetailer:{
+      locator: '#all-retailers > div > div:nth-child(5) > a',
+      retailerName: 'Walmart',
+      source: 'registry grid'
+    },
+    babyEarthRetailer:{
+      locator: '#all-retailers > div > div:nth-child(6) > a',
+      retailerName: 'BabyEarth',
+      source: 'registry grid'
+    },
+    crowdriseRetailer:{
+      locator: '#all-retailers > div > div:nth-child(7) > a',
+      retailerName: 'Crowdrise Baby',
+      source: 'registry grid'
+    },
+    targetRetailer:{
+      locator: '#all-retailers > div > div:nth-child(8) > a',
+      retailerName: 'Target',
+      source: 'registry grid'
+    }
+  }
+}
--- helper/searchHAR.js
+++ helper/searchHAR.js
@@ -1,3 +1,5 @@
+'use strict';
+
 module.exports = {
     findEvent: function(entries, event) {
         var eventFound = false;
@@ -10,6 +12,31 @@ module.exports = {
             }
         }
         return eventFound;
+    },
+
+    eventNum: function(entries){
+        var index = 0;
+        var postData;
+        for(var i=0; i < entries.length; i++) {
+            postData = entries[i].request.postData;
+            if(!!postData && JSON.stringify(postData.text.replace(/"+/g, '')).includes('event:Feed Card Impression'))  {
+                index += 1
+            }
+        }
+        return index
+    },
+    
+    findUrl: function(entries,expectUrl){
+        var urlFound = false;
+        var url;
+        for (var i=0; i < entries.length; i++){
+            url = entries[i].request.url
+            if(!!url && url===expectUrl){
+               urlFound = true;
+               break; 
+            }
+        }
+        return urlFound
     },
 
     findProperty: function(entries, property, propertyValue) {
